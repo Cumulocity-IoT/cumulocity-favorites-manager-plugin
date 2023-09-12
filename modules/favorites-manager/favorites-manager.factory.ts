2bc09bc761bf61e7b0a5a1950ec7d158a21cf74e
Christian Guether <christian.guether@softwareag.com>
2023-09-12 11:28:14 +0200

    add additional translations

--- modules/favorites-manager/favorites-manager.factory.ts
+++ modules/favorites-manager/favorites-manager.factory.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { NavigatorNode, NavigatorNodeFactory, gettext } from '@c8y/ngx-components';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class FavoritesManagerNavigationFactory implements NavigatorNodeFactory {
+  constructor(private translateService: TranslateService) {}
+
   private readonly FAVORITES_LIST_NAVIGATOR_NODE = new NavigatorNode({
-    label: gettext('Favorites'),
+    label: this.translateService.instant('favorites.title'),
     path: '/favorites',
     icon: 'mark-as-favorite',
     priority: 5,
