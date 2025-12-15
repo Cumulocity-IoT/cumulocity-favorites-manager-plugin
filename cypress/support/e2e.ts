8388fac0182a14157e888a8aa0a073e83fabf432
Christian Guether <christian.guether@cumulocity.com>
2025-12-15 11:53:38 +0100

    revert to clean state

--- cypress/support/e2e.ts
+++
@@ -1,9 +0,0 @@
-import 'cumulocity-cypress/lib/commands';
-import { registerCommands } from './commands';
-
-registerCommands();
-
-import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector';
-
-installLogsCollector();
-
